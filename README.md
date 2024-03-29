5af35331130daa4419b2f990bcd5e5323919b631
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2024-03-29 14:31:44 -0700

    cleanup files

--- README.md
+++ README.md
@@ -2,19 +2,31 @@
 
 Proof-of-concept MVP blog built with MDX and [Docusaurus](https://docusaurus.io/), hosted for free on [GitHub pages](https://pages.github.com/)
 
-### Local Development
+## Local Development
 
-```
-$ yarn
-$ yarn start
+```sh
+$ npm install
+$ npm start
 ```
 
 This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
 
-### Build
+## Production Build
 
-```
-$ yarn build
+```sh
+$ npm run build
 ```
 
 This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+## Features
+
+- **Automated Image Optimizer:** a [GitHub Actions](https://github.com/features/actions) workflow using [CalibreApp](https://github.com/calibreapp/image-actions) to compress images on every pull request
+- **Automated Deployments:** free hosting and automated CD with [GitHub pages](https://pages.github.com/)
+- **Auto-generated Website:** [Docusaurus](https://docusaurus.io/) to create static files for a modern blog UI
+- **Linter:** pre-commit hook to enforce consistent Markdown formatting
+
+## Specifications
+
+- Launched: March 2023
+- url: https://spencerlepine.github.io/blog
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,23 +4,23 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Spencer Lepine',
-  tagline: 'Spencer Lepine | Full Stack Developer',
+  tagline: 'Spencer Lepine | Software Engineer',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://spenlep-amzn.github.io',
+  url: 'https://spencer.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/blog',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'spenlep-amzn', // Usually your GitHub org/user name.
+  organizationName: 'spencerlepine', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
   trailingSlash: false,
 
@@ -45,8 +45,9 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/spencerlepine/blog',
+          editUrl: 'https://github.com/spencerlepine/blog',
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -59,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card-thumbnail.jpg',
       navbar: {
         title: 'Spencer Lepine',
         logo: {
